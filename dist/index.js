c01e1e7b17cdb549876d1aab416aecb66055f2f7
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2025-03-24 23:38:18 +0600

    added typescript basics code

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,50 @@ if (age > 20) {
     age = age + 1;
 }
 console.log(age);
+let a = [1, 2, 3, 4, 5];
+console.log(a[0]);
+let person = { name: "Tahsin", age: 22 };
+console.log(person.age);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
+//any
+let value = 5;
+value = "Tahsin";
+console.log(value);
+//enum
+//role based authentication
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+    Role[Role["Guest"] = 2] = "Guest";
+})(Role || (Role = {}));
+let userRole = Role.Admin;
+console.log(userRole);
+//tuple
+let tup = ["Tahsin", 22];
+console.log(tup[0]);
+let studentId = 123;
+console.log(studentId);
+let guestId = "abc";
+console.log(guestId);
+let adminId = true;
+console.log(adminId);
+//type union
+let tu;
+tu = "Tahsin";
+console.log(tu);
+tu = 22;
+console.log(tu);
+let tu1;
+tu1 = "success";
+console.log(tu1);
+let G = {
+    name: "Tahsin Interface",
+    age: 22
+};
+console.log(G);
+console.log(G.name);
 //# sourceMappingURL=index.js.map
--- ts_intro/src/index.ts
+++ ts_intro/src/index.ts
@@ -8,3 +8,67 @@ if (age > 20) {
 }
 
 console.log(age)
+
+
+let a:number[] = [1,2,3,4,5]
+console.log(a[0]);
+
+let person: {name: string; age: number} = {name: "Tahsin", age: 22}
+console.log(person.age);
+
+function add(a: number, b: number): number {
+    return a + b
+}
+console.log(add(2,3));
+
+//any
+let value: any = 5
+value = "Tahsin"
+console.log(value);
+
+
+//enum
+//role based authentication
+enum Role {Admin, User, Guest}
+let userRole: Role = Role.Admin
+console.log(userRole);
+
+//tuple
+let tup: [string, number] = ["Tahsin", 22]
+console.log(tup[0]);
+
+//type alias
+type id = number | string | boolean
+let studentId: id = 123
+console.log(studentId);
+let guestId: id = "abc"
+console.log(guestId);
+let adminId: id = true
+console.log(adminId);
+
+//type union
+let tu: string | number
+tu = "Tahsin"
+console.log(tu);
+tu = 22
+console.log(tu);
+
+let tu1: "success" | "error"
+tu1 = "success"
+console.log(tu1);
+//tu1 = "pending" error code
+
+
+//interface
+interface Iuser {
+    name: string,
+    age: number
+}
+
+let G: Iuser = {
+    name: "Tahsin Interface",
+    age: 22
+}
+
+console.log(G);
+console.log(G.name);
